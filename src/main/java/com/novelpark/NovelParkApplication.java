73a4bc9dc906225217ec6fbf90de2acc8a184138
Jeongyong Park <1239824@naver.com>
2023-08-09 00:12:39 +0900

    setting: 초기 개발 환경 세팅

---
+++ src/main/java/com/novelpark/NovelParkApplication.java
@@ -0,0 +1,12 @@
+package com.novelpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelParkApplication.class, args);
+	}
+}
