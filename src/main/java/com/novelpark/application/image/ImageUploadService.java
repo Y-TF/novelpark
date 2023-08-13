cbdbe539960f49ee383efaaf03fdf0441cf694fa
Jeongyong Park <1239824@naver.com>
2023-08-13 23:46:53 +0900

    style: 기본 프로필 이미지 URL 환경변수 이름 수정

--- src/main/java/com/novelpark/application/image/ImageUploadService.java
+++ src/main/java/com/novelpark/application/image/ImageUploadService.java
@@ -1,22 +1,20 @@
 package com.novelpark.application.image;
 
+import com.novelpark.domain.image.ImageFile;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.novelpark.domain.image.ImageFile;
-
-import lombok.RequiredArgsConstructor;
-
 @RequiredArgsConstructor
 @Service
 public class ImageUploadService {
 
-	@Value("${url.profile}")
-	private String defaultProfileUrl;
-
 	private final S3Uploader s3Uploader;
+
+	@Value("${s3.default.profile-url}")
+	private String defaultProfileUrl;
 
 	@Transactional
 	public String uploadImage(MultipartFile image) {
